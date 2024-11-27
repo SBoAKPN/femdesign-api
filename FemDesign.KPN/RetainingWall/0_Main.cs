314e9bc8da9e48aaeefb9fa53e38a82553ea2bbf
SBoAKPN <kristoffer.persson@structor.se>
2024-11-27 18:03:22 +0100

    Element class inplemented

--- FemDesign.KPN/RetainingWall/0_Main.cs
+++ FemDesign.KPN/RetainingWall/0_Main.cs
@@ -16,6 +16,7 @@ using FemDesign.Releases;
 using FemDesign.Results;
 using FemDesign.Shells;
 using FemDesign.Utils;
+using RetainingWall;
 using StruSoft.Interop.StruXml.Data;
 
 namespace FemDesign.RetainingWall
@@ -24,96 +25,102 @@ namespace FemDesign.RetainingWall
     {
         static void Main()
         {
-            // Parametric design of a retainingwall v0.0
+            //// Parametric design of a retainingwall v0.0
 
-            // ----- Define Strucutre -----
+            //// ----- Define Strucutre -----
 
-            // ----- INDATA -----
+            //// ----- INDATA -----
 
-            // Corner points of BPL (part1)
-            // X-kord , Y-kord , Z-kord, thickness
-            var PP1_0 = new List<double> { 0, 0, 0, 1.0 };
-            var PP1_1 = new List<double> { 4.0, 0, 0, 0.9 };
-            var PP1_2 = new List<double> { 4.0, 1.0, 0, 0.9 };
-            var PP1_3 = new List<double> { 0, 1.0, 0 };
-            // Corner points of MUR (parSlabThickness2)
-            // X-kord [0] , Y-kord [1], Z-kord [2], thickness [3]
-            var PP2_0 = new List<double> { 0.5 + 1.0 / 2, 0, 6.0, 0.5 };
-            var PP2_1 = new List<double> { 0.5 + 1.0 / 2, 0, PP1_0[3], 1.0 };
-            var PP2_2 = new List<double> { 0.5 + 1.0 / 2, 1, PP1_0[3], 1.0 };
-            var PP2_3 = new List<double> { 0.5 + 1.0 / 2, 1, 6.0 };
+            //// Corner points of BPL (part1)
+            //// X-kord , Y-kord , Z-kord, thickness
+            //var PP1_0 = new List<double> { 0, 0, 0, 1.0 };
+            //var PP1_1 = new List<double> { 4.0, 0, 0, 0.9 };
+            //var PP1_2 = new List<double> { 4.0, 1.0, 0, 0.9 };
+            //var PP1_3 = new List<double> { 0, 1.0, 0 };
+            //// Corner points of MUR (parSlabThickness2)
+            //// X-kord [0] , Y-kord [1], Z-kord [2], thickness [3]
+            //var PP2_0 = new List<double> { 0.5 + 1.0 / 2, 0, 6.0, 0.5 };
+            //var PP2_1 = new List<double> { 0.5 + 1.0 / 2, 0, PP1_0[3], 1.0 };
+            //var PP2_2 = new List<double> { 0.5 + 1.0 / 2, 1, PP1_0[3], 1.0 };
+            //var PP2_3 = new List<double> { 0.5 + 1.0 / 2, 1, 6.0 };
 
-            // Styvhet för upplag
-            // KxNeg [0] , KxPos [1], KyNeg [2] , KyPos [3], KzNeg [4] , KzPos [5]
-            // Neg = compression, Pos = Tension
-            var KSupp = new List<double> { 5e3, 5e3, 5e3, 5e3, 10e3, 0 };
+            //// Styvhet för upplag
+            //// KxNeg [0] , KxPos [1], KyNeg [2] , KyPos [3], KzNeg [4] , KzPos [5]
+            //// Neg = compression, Pos = Tension
+            //var KSupp = new List<double> { 5e3, 5e3, 5e3, 5e3, 10e3, 0 };
 
-            double creepUls = 0.5;
-            double creepSlq = 0.5;
-            double creepSlf = 0.5;
-            double creepSlc = 0.5;
-            double shrinkage = 0.5;
+            //double creepUls  = 0.1;
+            //double creepSlq  = 0.1;
+            //double creepSlf  = 0.1;
+            //double creepSlc  = 0.1;
+            //double shrinkage = 0.1;
 
-            // ------------------
+            //// ------------------
 
-            // Corner points of Slab1 BPL 
-            var P1_0 = new Point3d(PP1_0[0], PP1_0[1], PP1_0[2]);
-            var P1_1 = new Point3d(PP1_1[0], PP1_1[1], PP1_1[2]);
-            var P1_2 = new Point3d(PP1_2[0], PP1_2[1], PP1_2[2]);
-            var P1_3 = new Point3d(PP1_3[0], PP1_3[1], PP1_3[2]);
-            // Points for thickness
-            var PointsBPL = new List<Point3d> { P1_0, P1_1, P1_2 };
-            var SlabThicknessBPL = new List<double> { PP1_0[3], PP1_1[3], PP1_2[3] };
+            //// Corner points of Slab1 BPL 
+            //var P1_0 = new Point3d(PP1_0[0], PP1_0[1], PP1_0[2]);
+            //var P1_1 = new Point3d(PP1_1[0], PP1_1[1], PP1_1[2]);
+            //var P1_2 = new Point3d(PP1_2[0], PP1_2[1], PP1_2[2]);
+            //var P1_3 = new Point3d(PP1_3[0], PP1_3[1], PP1_3[2]);
+            //// Points for thickness
+            //var PointsBPL = new List<Point3d> { P1_0, P1_1, P1_2 };
+            //var SlabThicknessBPL = new List<double> { PP1_0[3], PP1_1[3], PP1_2[3] };
 
-            // Corner points of Slab2 MUR 
-            var P2_0 = new Point3d(PP2_0[0], PP2_0[1], PP2_0[2]);
-            var P2_1 = new Point3d(PP2_1[0], PP2_1[1], PP2_1[2]);
-            var P2_2 = new Point3d(PP2_2[0], PP2_2[1], PP2_2[2]);
-            var P2_3 = new Point3d(PP2_3[0], PP2_3[1], PP2_3[2]);
-            // Points for thickness
-            var PointsMur = new List<Point3d> { P2_0, P2_1, P2_2 };
-            var SlabThicknessMur = new List<double> { PP2_0[3], PP2_1[3], PP2_2[3] };
+            //// Corner points of Slab2 MUR 
+            //var P2_0 = new Point3d(PP2_0[0], PP2_0[1], PP2_0[2]);
+            //var P2_1 = new Point3d(PP2_1[0], PP2_1[1], PP2_1[2]);
+            //var P2_2 = new Point3d(PP2_2[0], PP2_2[1], PP2_2[2]);
+            //var P2_3 = new Point3d(PP2_3[0], PP2_3[1], PP2_3[2]);
+            //// Points for thickness
+            //var PointsMur = new List<Point3d> { P2_0, P2_1, P2_2 };
+            //var SlabThicknessMur = new List<double> { PP2_0[3], PP2_1[3], PP2_2[3] };
 
-            // Corner points of Slab 3 BPL-MUR
-            // X-kord , Y-kord , Z-kord, thickness
-            var PP3_0 = new List<double> { PP2_1[0], PP2_1[1], PP2_1[2], PP1_0[3] };
-            var PP3_1 = new List<double> { PP2_1[0], PP2_1[1], PP1_0[2], PP1_0[3] };
-            var PP3_2 = new List<double> { PP2_2[0], PP2_2[1], PP1_3[2], PP1_0[3] };
-            var PP3_3 = new List<double> { PP2_2[0], PP2_2[1], PP2_2[2] };
+            //// Corner points of Slab 3 BPL-MUR
+            //// X-kord , Y-kord , Z-kord, thickness
+            //var PP3_0 = new List<double> { PP2_1[0], PP2_1[1], PP2_1[2], PP1_0[3] };
+            //var PP3_1 = new List<double> { PP2_1[0], PP2_1[1], PP1_0[2], PP1_0[3] };
+            //var PP3_2 = new List<double> { PP2_2[0], PP2_2[1], PP1_3[2], PP1_0[3] };
+            //var PP3_3 = new List<double> { PP2_2[0], PP2_2[1], PP2_2[2] };
 
-            // Corner points of Slab3 (Point) 
-            var P3_0 = new Point3d(PP3_0[0], PP3_0[1], PP3_0[2]);
-            var P3_1 = new Point3d(PP3_1[0], PP3_1[1], PP3_1[2]);
-            var P3_2 = new Point3d(PP3_2[0], PP3_2[1], PP3_2[2]);
-            var P3_3 = new Point3d(PP3_3[0], PP3_3[1], PP3_3[2]);
+            //// Corner points of Slab3 (Point) 
+            //var P3_0 = new Point3d(PP3_0[0], PP3_0[1], PP3_0[2]);
+            //var P3_1 = new Point3d(PP3_1[0], PP3_1[1], PP3_1[2]);
+            //var P3_2 = new Point3d(PP3_2[0], PP3_2[1], PP3_2[2]);
+            //var P3_3 = new Point3d(PP3_3[0], PP3_3[1], PP3_3[2]);
 
-            //Define properties
-            var materialDatabase = FemDesign.Materials.MaterialDatabase.GetDefault();
-            Material materialBtg = materialDatabase.MaterialByName("C35/45");
-            //var materialBtgNoEG = materialDatabase.MaterialByName("C35/45NoEG");
+            ////Define properties
+            //var materialDatabase = FemDesign.Materials.MaterialDatabase.GetDefault();
+            //Material materialBtg = materialDatabase.MaterialByName("C35/45");
+            ////var materialBtgNoEG = materialDatabase.MaterialByName("C35/45NoEG");
 
-            Material materialBtgNoMass = materialBtg.DeepClone();
-            materialBtgNoMass.Concrete.Mass = 0;
-            materialBtgNoMass.Name = "C35/45NoMass";
-            materialBtgNoMass.Guid = System.Guid.NewGuid();
+            //Material materialBtgNoMass = materialBtg.DeepClone();
+            //materialBtgNoMass.Concrete.Mass = 0;
+            //materialBtgNoMass.Name = "C35/45NoMass";
+            //materialBtgNoMass.Guid = System.Guid.NewGuid();
 
-            materialBtg = FemDesign.Materials.Material.ConcreteMaterialProperties(materialBtg, creepUls, creepSlq, creepSlf, creepSlc, shrinkage);
+            //materialBtg = FemDesign.Materials.Material.ConcreteMaterialProperties(materialBtg, creepUls, creepSlq, creepSlf, creepSlc, shrinkage);
 
-            // Define elements
-            var SlabAlignBottom = new ShellEccentricity(VerticalAlignment.Bottom, 0, false, false);
-            var SlabBPL = FemDesign.Shells.Slab.FromFourPoints(P1_0, P1_1, P1_2, P1_3, PP1_0[3], materialBtg, EdgeConnection.Default, SlabAlignBottom, null, "BPL");
-            var SlabMur = FemDesign.Shells.Slab.FromFourPoints(P2_0, P2_1, P2_2, P2_3, PP2_0[3], materialBtg, EdgeConnection.Default, null, null, "Mur");
-            var SlabBPLMur = FemDesign.Shells.Slab.FromFourPoints(P3_0, P3_1, P3_2, P3_3, PP3_0[3], materialBtgNoMass, EdgeConnection.Default, null, null, "BPL-Mur");
+            //// Define elements
+            //var SlabAlignBottom = new ShellEccentricity(VerticalAlignment.Bottom, 0, false, false);
+            //var SlabBPL = FemDesign.Shells.Slab.FromFourPoints(P1_0, P1_1, P1_2, P1_3, PP1_0[3], materialBtg, EdgeConnection.Default, SlabAlignBottom, null, "BPL");
+            //var SlabMur = FemDesign.Shells.Slab.FromFourPoints(P2_0, P2_1, P2_2, P2_3, PP2_0[3], materialBtg, EdgeConnection.Default, null, null, "Mur");
+            //var SlabBPLMur = FemDesign.Shells.Slab.FromFourPoints(P3_0, P3_1, P3_2, P3_3, PP3_0[3], materialBtgNoMass, EdgeConnection.Default, null, null, "BPL-Mur");
 
-            SlabBPL.UpdateThickness(PointsBPL, SlabThicknessBPL);
-            SlabMur.UpdateThickness(PointsMur, SlabThicknessMur);
+            //SlabBPL.UpdateThickness(PointsBPL, SlabThicknessBPL);
+            //SlabMur.UpdateThickness(PointsMur, SlabThicknessMur);
 
-            // Define support   
-            var regionBPL = SlabBPL.Region;
+            //// Define support   
+            //var regionBPL = SlabBPL.Region;
 
-            var motionsUpplagBPL = new FemDesign.Releases.Motions(KSupp[0], KSupp[1], KSupp[2], KSupp[3], KSupp[4], KSupp[5]);
+            //var motionsUpplagBPL = new FemDesign.Releases.Motions(KSupp[0], KSupp[1], KSupp[2], KSupp[3], KSupp[4], KSupp[5]);
 
-            var SurfaceSupport1 = new FemDesign.Supports.SurfaceSupport(regionBPL, motionsUpplagBPL, "BPL_Upplag");
+            //var SurfaceSupport1 = new FemDesign.Supports.SurfaceSupport(regionBPL, motionsUpplagBPL, "BPL_Upplag");
+
+            double MeshSize = 0.5;
+            var slabs = Element.slabs(MeshSize);
+            var supports = Element.supports(slabs);
+            var elements = Element.elements(slabs,supports);
+
 
             // ----- Define load cases -----
             LoadCase loadCaseDL = new LoadCase("DL", LoadCaseType.DeadLoad, LoadCaseDuration.Permanent);
