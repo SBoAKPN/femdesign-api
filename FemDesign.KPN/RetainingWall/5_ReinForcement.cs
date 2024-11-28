bc79394f380d9a755b98297de511735147eb6512
SBoAKPN <kristoffer.persson@structor.se>
2024-11-28 17:08:05 +0100

    Restructure of code with seperate classes complete

--- FemDesign.KPN/RetainingWall/5_ReinForcement.cs
+++ FemDesign.KPN/RetainingWall/5_ReinForcement.cs
@@ -4,9 +4,200 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using FemDesign;
+using FemDesign.Geometry;
+using FemDesign.Shells;
+using FemDesign.Reinforcement;
+using FemDesign.GenericClasses;
+
 namespace RetainingWall
 {
-    internal class _5_ReinForcement
+    internal class ReinForcement
     {
+        public static new List<FemDesign.GenericClasses.IStructureElement> reinfElements(List<Slab> slabs)
+        {
+            // Predefinition from input data
+            // slabs
+            Slab SlabBPL = slabs[0];
+            Slab SlabMUR = slabs[1];
+
+            var P_BPL = SlabBPL.SlabPart.Region.Contours[0].Points;
+            var P_MUR = SlabMUR.SlabPart.Region.Contours[0].Points;
+
+            // ----- INDATA -----
+
+            var reinforcementMat = FemDesign.Materials.MaterialDatabase.GetDefault().MaterialByName("B500B");
+            double diameter_BasArm_xB_BPL = 20e-3;
+            double diameter_BasArm_xT_BPL = 20e-3;
+            double diameter_BasArm_yB_BPL = 20e-3;
+            double diameter_BasArm_yT_BPL = 20e-3;
+
+            double diameter_ExtraArm_xB_BPL = 16e-3;
+            double diameter_ExtraArm_xT_BPL = 16e-3;
+            double diameter_ExtraArm_yB_BPL = 16e-3;
+            double diameter_ExtraArm_yT_BPL = 16e-3;
+
+            double cover_bottom_BPL = 100e-3;
+            double cover_topp_BPL = 55e-3;
+
+            var space_BasArm_BPL_xB_BPL = 0.20;
+            var space_BasArm_BPL_xT_BPL = 0.20;
+            var space_BasArm_BPL_yB_BPL = 0.20;
+            var space_BasArm_BPL_yT_BPL = 0.20;
+
+            var space_ExtraArm_BPL_xB_BPL = 0.40;
+            var space_ExtraArm_BPL_xT_BPL = 0.40;
+            var space_ExtraArm_BPL_yB_BPL = 0.40;
+            var space_ExtraArm_BPL_yT_BPL = 0.40;
+
+            Region region_BasArm_BPL = SlabBPL.Region;
+
+            var P_ExtraArm_BPL = new List<Point3d>
+            {
+                new Point3d(0   ,0   ,0),
+                new Point3d(2.5 ,0   ,0),
+                new Point3d(2.5 ,1.0 ,0),
+                new Point3d(0   ,1.0 ,0),
+            };
+
+            double diameter_BasArm_xB_MUR = 20e-3;
+            double diameter_BasArm_xT_MUR = 20e-3;
+            double diameter_BasArm_yB_MUR = 20e-3;
+            double diameter_BasArm_yT_MUR = 20e-3;
+
+            double diameter_ExtraArm_xB_MUR = 16e-3;
+            double diameter_ExtraArm_xT_MUR = 16e-3;
+            double diameter_ExtraArm_yB_MUR = 16e-3;
+            double diameter_ExtraArm_yT_MUR = 16e-3;
+
+            double cover_bottom_MUR = 100e-3;
+            double cover_topp_MUR = 55e-3;
+
+            var space_BasArm_MUR_xB_MUR = 0.20;
+            var space_BasArm_MUR_xT_MUR = 0.20;
+            var space_BasArm_MUR_yB_MUR = 0.20;
+            var space_BasArm_MUR_yT_MUR = 0.20;
+
+            var space_ExtraArm_MUR_xB_MUR = 0.40;
+            var space_ExtraArm_MUR_xT_MUR = 0.40;
+            var space_ExtraArm_MUR_yB_MUR = 0.40;
+            var space_ExtraArm_MUR_yT_MUR = 0.40;
+
+            Region region_BasArm_MUR = SlabMUR.Region;
+
+            var P_ExtraArm_MUR = new List<Point3d>
+            {
+                new Point3d(P_MUR[1].X    ,0   , 3.5),
+                new Point3d(P_MUR[1].X    ,0   , P_MUR[1].Z),
+                new Point3d(P_MUR[2].X    ,1.0 , P_MUR[2].Z),
+                new Point3d(P_MUR[2].X    ,1.0 , 3.5),
+            };
+
+            // ------------------
+
+            // ---- BPL ----
+
+            Region region_ExtraArm_BPL = new FemDesign.Geometry.Region(P_ExtraArm_BPL, Plane.XY);
+
+            var wire_BasArm_xB_BPL = new Wire(diameter_BasArm_xB_BPL, reinforcementMat, WireProfileType.Ribbed);
+            var wire_BasArm_xT_BPL = new Wire(diameter_BasArm_xT_BPL, reinforcementMat, WireProfileType.Ribbed);
+            var wire_BasArm_yB_BPL = new Wire(diameter_BasArm_yB_BPL, reinforcementMat, WireProfileType.Ribbed);
+            var wire_BasArm_yT_BPL = new Wire(diameter_BasArm_yT_BPL, reinforcementMat, WireProfileType.Ribbed);
+
+            var wire_ExtraArm_xB_BPL = new Wire(diameter_ExtraArm_xB_BPL, reinforcementMat, WireProfileType.Ribbed);
+            var wire_ExtraArm_xT_BPL = new Wire(diameter_ExtraArm_xT_BPL, reinforcementMat, WireProfileType.Ribbed);
+            var wire_ExtraArm_yB_BPL = new Wire(diameter_ExtraArm_yB_BPL, reinforcementMat, WireProfileType.Ribbed);
+            var wire_ExtraArm_yT_BPL = new Wire(diameter_ExtraArm_yT_BPL, reinforcementMat, WireProfileType.Ribbed);
+
+            var BasArm_xB_BPL = new Straight(ReinforcementDirection.X, space_BasArm_BPL_xB_BPL, FemDesign.GenericClasses.Face.Bottom, cover_bottom_BPL);
+            var BasArm_xT_BPL = new Straight(ReinforcementDirection.X, space_BasArm_BPL_xT_BPL, FemDesign.GenericClasses.Face.Top, cover_topp_BPL);
+            var BasArm_yB_BPL = new Straight(ReinforcementDirection.Y, space_BasArm_BPL_yB_BPL, FemDesign.GenericClasses.Face.Bottom, cover_bottom_BPL);
+            var BasArm_yT_BPL = new Straight(ReinforcementDirection.Y, space_BasArm_BPL_yT_BPL, FemDesign.GenericClasses.Face.Top, cover_topp_BPL);
+
+            var ExtraArm_xB_BPL = new Straight(ReinforcementDirection.X, space_ExtraArm_BPL_xB_BPL, FemDesign.GenericClasses.Face.Bottom, cover_bottom_BPL);
+            var ExtraArm_xT_BPL = new Straight(ReinforcementDirection.X, space_ExtraArm_BPL_xT_BPL, FemDesign.GenericClasses.Face.Top, cover_topp_BPL);
+            var ExtraArm_yB_BPL = new Straight(ReinforcementDirection.Y, space_ExtraArm_BPL_yB_BPL, FemDesign.GenericClasses.Face.Bottom, cover_bottom_BPL);
+            var ExtraArm_yT_BPL = new Straight(ReinforcementDirection.Y, space_ExtraArm_BPL_yT_BPL, FemDesign.GenericClasses.Face.Top, cover_topp_BPL);
+
+            var ArmList_BPL = new List<Straight>
+            {
+                BasArm_xB_BPL,
+                BasArm_xT_BPL,
+                BasArm_yB_BPL,
+                BasArm_yT_BPL,
+                ExtraArm_xB_BPL,
+                ExtraArm_xT_BPL,
+                ExtraArm_yB_BPL,
+                ExtraArm_yT_BPL,
+            };
+
+            var srfReinf_BPL = new List<SurfaceReinforcement>
+            {
+                SurfaceReinforcement.DefineStraightSurfaceReinforcement(region_BasArm_BPL,   BasArm_xB_BPL, wire_BasArm_xB_BPL),
+                SurfaceReinforcement.DefineStraightSurfaceReinforcement(region_BasArm_BPL,   BasArm_xT_BPL, wire_BasArm_xT_BPL),
+                SurfaceReinforcement.DefineStraightSurfaceReinforcement(region_BasArm_BPL,   BasArm_yB_BPL, wire_BasArm_yB_BPL),
+                SurfaceReinforcement.DefineStraightSurfaceReinforcement(region_BasArm_BPL,   BasArm_yT_BPL, wire_BasArm_yT_BPL),
+
+                SurfaceReinforcement.DefineStraightSurfaceReinforcement(region_ExtraArm_BPL, ExtraArm_xB_BPL, wire_ExtraArm_xB_BPL),
+                SurfaceReinforcement.DefineStraightSurfaceReinforcement(region_ExtraArm_BPL, ExtraArm_xT_BPL, wire_ExtraArm_xT_BPL),
+                SurfaceReinforcement.DefineStraightSurfaceReinforcement(region_ExtraArm_BPL, ExtraArm_yB_BPL, wire_ExtraArm_yB_BPL),
+                SurfaceReinforcement.DefineStraightSurfaceReinforcement(region_ExtraArm_BPL, ExtraArm_yT_BPL, wire_ExtraArm_yT_BPL),
+            };
+
+            var reinfBPL = FemDesign.Reinforcement.SurfaceReinforcement.AddReinforcementToSlab(SlabBPL, srfReinf_BPL);
+
+            // ---- MUR ----
+
+            Region region_ExtraArm_MUR = new FemDesign.Geometry.Region(P_ExtraArm_MUR, Plane.YZ);
+
+            var wire_BasArm_xB_MUR = new Wire(diameter_BasArm_xB_MUR, reinforcementMat, WireProfileType.Ribbed);
+            var wire_BasArm_xT_MUR = new Wire(diameter_BasArm_xT_MUR, reinforcementMat, WireProfileType.Ribbed);
+            var wire_BasArm_yB_MUR = new Wire(diameter_BasArm_yB_MUR, reinforcementMat, WireProfileType.Ribbed);
+            var wire_BasArm_yT_MUR = new Wire(diameter_BasArm_yT_MUR, reinforcementMat, WireProfileType.Ribbed);
+
+            var wire_ExtraArm_xB_MUR = new Wire(diameter_ExtraArm_xB_MUR, reinforcementMat, WireProfileType.Ribbed);
+            var wire_ExtraArm_xT_MUR = new Wire(diameter_ExtraArm_xT_MUR, reinforcementMat, WireProfileType.Ribbed);
+            var wire_ExtraArm_yB_MUR = new Wire(diameter_ExtraArm_yB_MUR, reinforcementMat, WireProfileType.Ribbed);
+            var wire_ExtraArm_yT_MUR = new Wire(diameter_ExtraArm_yT_MUR, reinforcementMat, WireProfileType.Ribbed);
+
+            var BasArm_xB_MUR = new Straight(ReinforcementDirection.X, space_BasArm_MUR_xB_MUR, FemDesign.GenericClasses.Face.Bottom, cover_bottom_MUR);
+            var BasArm_xT_MUR = new Straight(ReinforcementDirection.X, space_BasArm_MUR_xT_MUR, FemDesign.GenericClasses.Face.Top, cover_topp_MUR);
+            var BasArm_yB_MUR = new Straight(ReinforcementDirection.Y, space_BasArm_MUR_yB_MUR, FemDesign.GenericClasses.Face.Bottom, cover_bottom_MUR);
+            var BasArm_yT_MUR = new Straight(ReinforcementDirection.Y, space_BasArm_MUR_yT_MUR, FemDesign.GenericClasses.Face.Top, cover_topp_MUR);
+
+            var ExtraArm_xB_MUR = new Straight(ReinforcementDirection.X, space_ExtraArm_MUR_xB_MUR, FemDesign.GenericClasses.Face.Bottom, cover_bottom_MUR);
+            var ExtraArm_xT_MUR = new Straight(ReinforcementDirection.X, space_ExtraArm_MUR_xT_MUR, FemDesign.GenericClasses.Face.Top, cover_topp_MUR);
+            var ExtraArm_yB_MUR = new Straight(ReinforcementDirection.Y, space_ExtraArm_MUR_yB_MUR, FemDesign.GenericClasses.Face.Bottom, cover_bottom_MUR);
+            var ExtraArm_yT_MUR = new Straight(ReinforcementDirection.Y, space_ExtraArm_MUR_yT_MUR, FemDesign.GenericClasses.Face.Top, cover_topp_MUR);
+
+            var ArmList_MUR = new List<Straight>
+            {
+                BasArm_xB_MUR,
+                BasArm_xT_MUR,
+                BasArm_yB_MUR,
+                BasArm_yT_MUR,
+                ExtraArm_xB_MUR,
+                ExtraArm_xT_MUR,
+                ExtraArm_yB_MUR,
+                ExtraArm_yT_MUR,
+            };
+
+            var srfReinf_MUR = new List<SurfaceReinforcement>
+            {
+                SurfaceReinforcement.DefineStraightSurfaceReinforcement(region_BasArm_MUR,   BasArm_xB_MUR, wire_BasArm_xB_MUR),
+                SurfaceReinforcement.DefineStraightSurfaceReinforcement(region_BasArm_MUR,   BasArm_xT_MUR, wire_BasArm_xT_MUR),
+                SurfaceReinforcement.DefineStraightSurfaceReinforcement(region_BasArm_MUR,   BasArm_yB_MUR, wire_BasArm_yB_MUR),
+                SurfaceReinforcement.DefineStraightSurfaceReinforcement(region_BasArm_MUR,   BasArm_yT_MUR, wire_BasArm_yT_MUR),
+
+                SurfaceReinforcement.DefineStraightSurfaceReinforcement(region_ExtraArm_MUR, ExtraArm_xB_MUR, wire_ExtraArm_xB_MUR),
+                SurfaceReinforcement.DefineStraightSurfaceReinforcement(region_ExtraArm_MUR, ExtraArm_xT_MUR, wire_ExtraArm_xT_MUR),
+                SurfaceReinforcement.DefineStraightSurfaceReinforcement(region_ExtraArm_MUR, ExtraArm_yB_MUR, wire_ExtraArm_yB_MUR),
+                SurfaceReinforcement.DefineStraightSurfaceReinforcement(region_ExtraArm_MUR, ExtraArm_yT_MUR, wire_ExtraArm_yT_MUR),
+            };
+
+            var reinfMUR = FemDesign.Reinforcement.SurfaceReinforcement.AddReinforcementToSlab(SlabMUR, srfReinf_MUR);
+            var reinfElements = new List<FemDesign.GenericClasses.IStructureElement> { reinfBPL, reinfMUR };
+            return reinfElements;
+        }
     }
 }
--- FemDesign.KPN/RetainingWall/6_Resultprocessing.cs
+++ FemDesign.KPN/RetainingWall/6_Resultprocessing.cs
@@ -3,10 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FemDesign;
+using FemDesign.Geometry;
+using FemDesign.Shells;
 
 namespace RetainingWall
 {
     internal class Resultprocessing
     {
+        public static List<INamedEntity> labelledsections(List<Slab> slabs)
+        {
+            // Predefinition from input data
+            // slabs
+            Slab SlabBPL = slabs[0];
+            Slab SlabMUR = slabs[1];
+
+            var P_BPL = SlabBPL.SlabPart.Region.Contours[0].Points;
+            var P_MUR = SlabMUR.SlabPart.Region.Contours[0].Points;
+
+            // ----- INDATA -----
+
+            var MUR_Poits = new List<Point3d>() { P_BPL[1], P_BPL[2], P_BPL[0], P_BPL[3] };
+
+            var LS_MUR_Poits = LabelledSectionInterpolate(MUR_Poits, 8);
+
+            var LS_MUR = new FemDesign.AuxiliaryResults.LabelledSection(LS_MUR_Poits, "LS_MUR");
+
+            // ------------------
+
+            var labelledsections = new List<INamedEntity> { LS_MUR };
+
+            return labelledsections;
+        }
+        public static List<Point3d> LabelledSectionInterpolate(List<Point3d> Pointlist, int InterpolatePoints)
+        {
+            // Create a list of linear interpolate values from a list with start/endpoints with given interpolate points
+
+
+            if (Pointlist.Count == 4 && InterpolatePoints > 0)
+            {
+                //Step size
+                Double[] Step0 =
+                {
+                    (Pointlist[2].X - Pointlist[0].X)/ InterpolatePoints,
+                    (Pointlist[2].Y - Pointlist[0].Y) / InterpolatePoints,
+                    (Pointlist[2].Z - Pointlist[0].Z) / InterpolatePoints
+                };
+
+                Double[] Step1 =
+                {
+                    (Pointlist[3].X - Pointlist[1].X) / InterpolatePoints,
+                    (Pointlist[3].Y - Pointlist[1].Y) / InterpolatePoints,
+                    (Pointlist[3].Z - Pointlist[1].Z) / InterpolatePoints
+                };
+
+                //Adding interpolate points
+                List<Point3d> PointlistInterpolate = new List<Point3d>();
+                for (int i = 0; i <= InterpolatePoints; i++)
+                {
+                    PointlistInterpolate.Add(new Point3d(Pointlist[0].X + Step0[0] * i, Pointlist[0].Y + Step0[1] * i, Pointlist[0].Z + Step0[2] * i));
+                    PointlistInterpolate.Add(new Point3d(Pointlist[1].X + Step0[1] * i, Pointlist[1].Y + Step1[1] * i, Pointlist[1].Z + Step1[2] * i));
+
+                }
+
+                return PointlistInterpolate;
+            }
+
+            else
+            {
+                throw new Exception("Pointlist should contain 4 points and InterpolatePoints > 0 ");
+            }
+
+        }
     }
 }
